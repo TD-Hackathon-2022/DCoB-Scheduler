84b22415ce5c689d55908592b7793f65a0b81c4e
zhangxuhai <lenshood.zxh@gmail.com>
2022-01-15 21:06:33 +0800

    feat: add dispather to dispatch msg by cmd

--- server.go
+++ server.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"github.com/TD-Hackathon-2022/DCoB-Scheduler/api"
 	. "github.com/TD-Hackathon-2022/DCoB-Scheduler/comm"
 	"github.com/gorilla/websocket"
+	"github.com/pkg/errors"
+	"google.golang.org/protobuf/proto"
 	"log"
+	"net"
 	"net/http"
 )
 
@@ -17,7 +21,7 @@ var (
 	logger = GetLogger()
 )
 
-func echo(w http.ResponseWriter, r *http.Request) {
+func handle(w http.ResponseWriter, r *http.Request) {
 	c, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		logger.Errorf("upgrade: %v", err)
@@ -26,14 +30,19 @@ func echo(w http.ResponseWriter, r *http.Request) {
 	defer c.Close()
 
 	for {
-		mt, message, err := c.ReadMessage()
+		mt, recvMsg, err := c.ReadMessage()
 		if err != nil {
 			logger.Errorf("read: %v", err)
 			break
 		}
 
-		GetLogger().Infof("recv: %s", message)
-		err = c.WriteMessage(mt, message)
+		sendMsg, err := dispatch(c.RemoteAddr(), mt, recvMsg)
+		if err != nil {
+			logger.Errorf("dispatch: %v", err)
+			break
+		}
+
+		err = c.WriteMessage(mt, sendMsg)
 		if err != nil {
 			logger.Errorf("write: %v", err)
 			break
@@ -41,9 +50,48 @@ func echo(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+var echoHandler = func(msg *api.Msg) *api.Msg { return msg }
+
+func dispatch(addr net.Addr, messageType int, data []byte) ([]byte, error) {
+	if messageType != websocket.BinaryMessage {
+		return nil, errors.New("wrong message type")
+	}
+
+	msg := &api.Msg{}
+	err := proto.Unmarshal(data, msg)
+	if err != nil {
+		return nil, errors.Wrap(err, "wrong message structure")
+	}
+
+	resMsg := &api.Msg{}
+	switch msg.Cmd {
+	case api.CMD_Register:
+		// TODO: handle
+		resMsg = echoHandler(msg)
+	case api.CMD_Close:
+		// TODO: handle
+		resMsg = echoHandler(msg)
+	case api.CMD_Status:
+		// TODO: handle
+		resMsg = echoHandler(msg)
+	case api.CMD_Assign:
+		// TODO: handle
+		resMsg = echoHandler(msg)
+	case api.CMD_Interrupt:
+		// TODO: handle
+		resMsg = echoHandler(msg)
+	default:
+		resMsg = echoHandler(msg)
+	}
+
+	marshaledData, err := proto.Marshal(resMsg)
+
+	return marshaledData, errors.Wrap(err, "marshal error")
+}
+
 const addr = ":8080"
 
 func main() {
-	http.HandleFunc("/", echo)
+	http.HandleFunc("/", handle)
 	log.Fatal(http.ListenAndServe(addr, nil))
 }
